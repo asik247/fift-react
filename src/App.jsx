c3274e025b15e38b1fe747b1a86c9b070a54f2f4
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-28 19:14:17 +0600

    project start code here now

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { add, diff, divided, divided as divs,  mult } from './Components/Mathematics/Math'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-
-  const sum = add(10,20)
-  const sub = diff(100,20)
-  const goonfal = mult(10,5)
-  const vag = divided(20,20)
-  const something = divs(100/200)
-  console.log(sum,sub,goonfal,something,vag);
-
-
-
-
-
-
-
-
-
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
--- src/Components/Mathematics/Math.jsx
+++
@@ -1,5 +0,0 @@
-const add = (a,b) =>a+b;
-const diff = (x,y) =>x-y;
-const multiplyTwoNumbers = (a,b) =>a*b;
-const divided = (a,b) =>a/b
-export{add,diff,multiplyTwoNumbers as mult,divided}
