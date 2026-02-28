b68cb0ca5fad9787f6f69becb3b72ab3b669135d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-01 00:47:28 +0600

    done card showing

--- src/Components/Bottol/Bottol.jsx
+++ src/Components/Bottol/Bottol.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
 const Bottol = ({ bottol }) => {
-    const { id, brand, type, color, price_usd, img_url, in_stock } = bottol;
+    const { id, brand, type, color, price_usd, img_url, in_stock}  = bottol;
     console.log(bottol);
     return (
         <div>
             <div className="card bg-base-100 w-96 shadow-sm">
                 <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
+                    <img src={img_url} alt="" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Card Title
+                      {id}
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
