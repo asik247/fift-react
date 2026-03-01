bbe87f347de59b078227a095fb1bc9d121ed651b
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-01 11:39:46 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import Bottols from './Components/Bottols/Bottols';
+import Navbar from './Components/Navbar/Navbar';
 
 const bottolsPromise = fetch("./Bottols.json")
 .then(res=>res.json())
@@ -7,6 +8,8 @@ console.log(bottolsPromise);
 const App = () => {
   return (
     <div>
+      {/* nav code here */}
+      <Navbar></Navbar>
       <Suspense fallback={<h1>Loadding....</h1>}>
         <Bottols bottolsPromise={bottolsPromise}></Bottols>
       </Suspense>
