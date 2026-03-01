c593f3adbe706a2956c468a0ca21c54653ec79a5
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-01 22:31:46 +0600

    now code start

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { Suspense } from 'react';
 import Bottols from './Components/Bottols/Bottols';
 import Navbar from './Components/Navbar/Navbar';
-
+// Bottoles promise fetch code start here;
 const bottolsPromise = fetch("./Bottols.json")
 .then(res=>res.json())
 console.log(bottolsPromise);
+// Bottoles promise fetch code end here;
+
 const App = () => {
   return (
     <div>
       {/* nav code here */}
       <Navbar></Navbar>
+      {/* suspense use for Bottols components */}
       <Suspense fallback={<h1>Loadding....</h1>}>
         <Bottols bottolsPromise={bottolsPromise}></Bottols>
       </Suspense>
